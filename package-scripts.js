de5ca51710ed9327a9d7473879f50914699921c1
Kent C. Dodds <kent+github@doddsfamily.us>
2017-04-13 11:46:22 -0600

    feat: upgrade prettier to 1.0.0! (#65)
    This adds support for the new `semi` and `useTabs` options!

--- package-scripts.js
+++ package-scripts.js
@@ -1,40 +1,76 @@
-/* eslint max-len:0 */
+const npsUtils = require('nps-utils')
+
+const series = npsUtils.series
+const concurrent = npsUtils.concurrent
+const rimraf = npsUtils.rimraf
+
 module.exports = {
   scripts: {
+    contributors: {
+      add: {
+        description: 'When new people contribute to the project, run this',
+        script: 'all-contributors add',
+      },
+      generate: {
+        description: 'Update the badge and contributors table',
+        script: 'all-contributors generate',
+      },
+    },
     commit: {
       description: 'This uses commitizen to help us generate well formatted commit messages',
       script: 'git-cz',
     },
-    test: {default: `jest --coverage`, watch: 'jest --watch'},
+    test: {
+      default: 'jest --coverage',
+      watch: 'jest --watch',
+    },
     build: {
       description: 'delete the dist directory and run babel to build the files',
-      script: 'rimraf dist && babel --copy-files --out-dir dist --ignore *.test.js,__mocks__ src',
+      script: series(
+        rimraf('dist'),
+        'babel --copy-files --out-dir dist --ignore *.test.js,__mocks__ src'
+      ),
     },
-    lint: {description: 'lint the entire project', script: 'eslint .'},
+    lint: {
+      description: 'lint the entire project',
+      script: 'eslint .',
+    },
     reportCoverage: {
       description: 'Report coverage stats to codecov. This should be run after the `test` script',
       script: 'codecov',
     },
     release: {
       description: 'We automate releases with semantic-release. This should only be run on travis',
-      script: 'semantic-release pre && npm publish && semantic-release post',
+      script: series(
+        'semantic-release pre',
+        'npm publish',
+        'semantic-release post'
+      ),
     },
     validate: {
       description: 'This runs several scripts to make sure things look good before committing or on clean install',
-      script: 'nps format && nps -p lint,build,test',
+      script: concurrent.nps('lint', 'build', 'test'),
     },
     format: {
       description: 'Formats everything with prettier-eslint',
       script: 'prettier-eslint src/*.js src/**/*.js --write',
     },
-    addContributor: {
-      description: 'When new people contribute to the project, run this',
-      script: 'all-contributors add',
-    },
-    generateContributors: {
-      description: 'Update the badge and contributors table',
-      script: 'all-contributors generate',
-    },
   },
-  options: {silent: false},
+  options: {
+    silent: false,
+  },
 }
+
+// this is not transpiled
+/*
+  eslint
+  max-len: 0,
+  comma-dangle: [
+    2,
+    {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      functions: 'never'
+    }
+  ]
+ */
