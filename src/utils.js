5a5ba559f7247b4f8e4e4a1f65cb96e4b94479f4
Oliver Joseph Ash <oliverjash@gmail.com>
2017-06-27 21:36:07 +0100

    Tidy up condition

--- src/utils.js
+++ src/utils.js
@@ -333,8 +333,8 @@ function makePrettierOption(
 ) {
   if (
     prettierRuleValue !== RULE_NOT_CONFIGURED &&
-    (typeof prettierRuleValue !== 'undefined' &&
-      prettierRuleValue !== RULE_DISABLED)
+    prettierRuleValue !== RULE_DISABLED &&
+    typeof prettierRuleValue !== 'undefined'
   ) {
     return prettierRuleValue
   }
