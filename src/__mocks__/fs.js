2d7709547a67776abbb008da6d55e04646db8360
Peter-Paul <mail@ppvg.nl>
2017-09-14 20:40:52 +0200

    fix(test): fix JSON parse errors in tests

--- src/__mocks__/fs.js
+++ src/__mocks__/fs.js
@@ -1,4 +1,8 @@
 const fs = require.requireActual('fs')
 module.exports = Object.assign({}, fs, {
-  readFileSync: jest.fn(() => 'var fake = true'),
+  readFileSync: jest.fn(filename => {
+    return /package\.json$/.test(filename) ?
+      '{"name": "fake", "version": "0.0.0"}' :
+      'var fake = true'
+  }),
 })
