a824c459c67191f884a59808ce59f4ac49eff76e
Xianming Zhong <chinesedfan@qq.com>
2019-06-12 17:03:28 +0800

    fix: failed CI due to prettier upgrade

--- src/__mocks__/fs.js
+++ src/__mocks__/fs.js
@@ -2,7 +2,7 @@ const fs = require.requireActual("fs");
 module.exports = Object.assign({}, fs, {
   readFileSync: jest.fn(filename => {
     if (/package\.json$/.test(filename)) {
-      return '{"name": "fake", "version": "0.0.0"}';
+      return '{"name": "fake", "version": "0.0.0", "prettier": {}}';
     } else if (/\.(j|t)s$/.test(filename)) {
       return "var fake = true";
     }
